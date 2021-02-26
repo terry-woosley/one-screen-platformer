5b81fc4b8656b1f90dcb94056b703f7c213c47fd
Terry <s526788@nwmissouri.edu>
2021-02-26 16:35:14 -0600

    Add Collectables and Goal
    Constructed level, added two collectibles with door animation, and added an exit to level.

--- Level.gd
+++ Level.gd
@@ -1,4 +1,39 @@
 extends Node2D
 
+var gem_flag = false
+var key_flag = false
+var key_door_has_played = false
+
+func _ready():
+	$KeyDoor.position = Vector2(55, 88)
+
+func _process(delta):
+	if Input.is_action_pressed("skip_level"):
+		print("Level Skipped")
+		gem_flag = true
+		key_flag = true
+		$Player.position = $LevelSkip.position
+	
+	if gem_flag == true and key_flag == true and key_door_has_played == false:
+		key_door_has_played = true
+		$KeyDoor/AnimationPlayer.play("OpenWithKey")
+
 func _on_DeathArea_body_entered(body):
 	$Player.position = $Spawn.position
+
+func _on_Gem_body_entered(body):
+	$Gem.hide()
+	gem_flag = true
+	print("You've got the gem!")
+
+func _on_Key_body_entered(body):
+	$Key.hide()
+	key_flag = true
+	print("You've got the key!")
+
+func _on_ExitDoor_body_entered(body):
+	if gem_flag == true and key_flag == true:
+		$Player.hide()
+		print("You've escaped with the goods!")
+	else:
+		print("You need the gem and the key!")
--- Player.gd
+++ Player.gd
@@ -11,16 +11,17 @@ var screensize
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	 screensize = get_viewport_rect().size
+	screensize = get_viewport_rect().size
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-
 	if Input.is_action_pressed("ui_right"):
 		velocity.x = move_toward(velocity.x, MAX_SPEED, ACCELERATION * delta)
+		$Sprite.flip_h = false
 	elif Input.is_action_pressed("ui_left"):
 		velocity.x = move_toward(velocity.x, -MAX_SPEED, ACCELERATION * delta)
+		$Sprite.flip_h = true
 	elif is_on_floor():
 		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
 
@@ -30,7 +31,10 @@ func _process(delta):
 func _physics_process(delta):
 	#falling
 	velocity.y += GRAVITY * delta
-	velocity = move_and_slide(velocity, Vector2.UP)
-	
+	if is_on_floor():
+		velocity = move_and_slide_with_snap(velocity, Vector2.DOWN, Vector2.UP, true)
+	else:
+		velocity = move_and_slide_with_snap(velocity, Vector2.ZERO, Vector2.UP, true)
+
 	position.x = clamp(position.x, 0, screensize.x)
 	position.y = clamp(position.y, 0, screensize.y)
